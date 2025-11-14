d5c35da68835179e1d6264b06112252de3c281d9
krishkumar0204 <krishsadipur@gmail.com>
2025-11-14 13:03:06 +0530

    adding listing

--- init/index.js
+++ init/index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const initData = require("../init/data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = process.env.ATLAS_DB_URL;
 
 main()
   .then((res) => {
